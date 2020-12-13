23a130255092c3dae1821c079defcda94791c755
Katie Carrel <katiecarrel@comcast.net>
2020-12-12 16:53:26 -0800

    added validation for item name

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :geocache_object, optional: true
+
+  validates :name, uniqueness: true, length: { maximum: 50}, format: { with: /\A[a-zA-Z0-9 ]+\z/,
+    message: "only allows letters, numbers and spaces" }
 end
