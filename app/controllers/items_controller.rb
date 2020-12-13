78cb59fe460a33fe8b20f3d6abba5c32363164c6
Katie Carrel <katiecarrel@comcast.net>
2020-12-12 17:04:35 -0800

    add endpoints for creating a new item and to update an items geocache object association

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+
+    def create 
+        item_params = params.require(:item).permit(:name, :geocache_object_id, :active_at, :inactive_at)
+        Item.create(item_params)
+    end
+
+    def update 
+        item_params = params.require(:item).permit(:geocache_object_id)
+        geocache_object = GeocacheObject.find(item_params[:geocache_object_id])
+        if geocache_object.items.count >= 3 
+            flash.now[:messages] = "A Geocache Object cannot contain 3 or more items."
+        else 
+            Item.update(item_params)
+        end
+    end
 end
--- app/models/geocache_object.rb
+++ app/models/geocache_object.rb
@@ -1,2 +1,3 @@
 class GeocacheObject < ApplicationRecord
+    has_many :items
 end
